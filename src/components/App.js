a9ac8ac29001dc075589c33dac3d0d83bbfb673f
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-07-20 20:11:03 +0300

    router-routes lab done

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,20 @@ import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
-
+import './App.css';
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
+
